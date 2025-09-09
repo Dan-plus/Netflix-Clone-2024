9e549a155718c2fb7110ce6d4e39626264099d07
Dan-plus <danielgena88@gmail.com>
2025-09-09 01:51:09 -0500

    header and footer

---
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../Commponents/Header/Header'
+import Footer from '../../Commponents/Footer/Footer'
+
+const Home = () => {
+  return (
+    <>
+        <Header/>
+        <Footer/>
+    </>
+  )
+}
+
+export default Home
